63218b76dea2c5979d4dca57415ce2f04937a728
Gyan Aryan <gyanaryan1519@gmail.com>
2026-06-19 18:40:18 +0530

    added route

--- ReactRouter/src/components/Header/Header.jsx
+++ ReactRouter/src/components/Header/Header.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import {NavLink , Link} from 'react-router-dom'
-
-
 export default function Header() {
   return (
       <header className="shadow sticky z-50 top-0">
@@ -53,7 +51,16 @@ export default function Header() {
                                   About
                               </NavLink>
                           </li>
-                          
+                          <li>
+                              <NavLink
+                              to="/contact"
+                                  className={() =>
+                                      `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                  }
+                              >
+                                  Contact
+                              </NavLink>
+                          </li>
                       </ul>
                   </div>
               </div>
--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -1,27 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom' 
+import { RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from 'react-router-dom' 
 import Layout from "../src/Layout/Layout"
 import './index.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import About from './components/About/About'
 import Home from './components/Home/Home'
-const router  = createBrowserRouter([
-  {
-    path:"/",
-    element:<Layout />,
-    errorElement: <div className="text-center text-3xl font-bold py-20 text-red-800">Oops! Page Not Found 🚫</div>,
-    children:[{
-      path:"",
-      element:<Home />
-    },
-    {
-      path:"",
-      element:<About/>
-    }]
-  }
-])
+import Contact from './components/Contact/Contact'
+
+
+// const router  = createBrowserRouter([
+//   {
+//     path:"/",
+//     element:<Layout />,
+//     errorElement: <div className="text-center text-3xl font-bold py-20 text-red-800">Oops! Page Not Found 🚫</div>,
+//     children:[{
+//       path:"",
+//       element:<Home />
+//     },
+//     {
+//       path:"about",
+//       element:<About/>
+//     },
+//   {
+//     path:"contact",
+//       element:<Contact/>
+//   }]
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path="" element={<Home />} />
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+    </Route>
+  )
+)
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
