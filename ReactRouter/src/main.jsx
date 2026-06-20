fa749c20c499d962a5ed923d3d9351f43d945d54
Gyan Aryan <gyanaryan1519@gmail.com>
2026-06-20 07:49:43 +0530

    updated main

--- ReactRouter/src/main.jsx
+++ ReactRouter/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// 1. Added 'Route' to the imports and removed unused ones like RouterProvider
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom' 
 import Layout from "../src/Layout/Layout"
 import './index.css'
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* 2. Moved the Layout to a parent Route component */}
+        
         <Route path='/' element={<Layout/>}>
           <Route path="" element={<Home />} />
           <Route path='about' element={<About/>}/>
