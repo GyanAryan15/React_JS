940516e24231081c65a283e3e298e451f57a521a
Gyan Aryan <gyanaryan1519@gmail.com>
2026-06-20 09:11:14 +0530

    Login added

---
+++ minicontext/src/context/userContext.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserContext = React.createContext()
+
+export default UserContext;
+
+<UserContext>
+  <login/>
+  <Card>
+    <Data />
+  </Card>
+</UserContext>
